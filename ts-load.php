9aab74db16f3364918e9161d67805b6954b9096f
Masteriii <masteriii_2@hotmail.com>
2021-01-04 14:37:42 +0700

    Add files via upload

--- ts-load.php
+++ ts-load.php
@@ -8,10 +8,10 @@ require_once $cur_dir.'/fn.php';
 //โค้ดเกมือนกันทุกอัน แต่วางต่างที่
 $arr_webservice = array(
 'ipspiderws'=>array('ep'=>'https://ipspiderws.herokuapp.com/ep.php')
-// ,'ipspiderws2'=>array('ep'=>'https://ipspiderws2.herokuapp.com/ep.php')
-// ,'ipspiderws3'=>array('ep'=>'https://ipspiderws3.herokuapp.com/ep.php')
-// ,'ipspiderws4'=>array('ep'=>'https://ipspiderws4.herokuapp.com/ep.php')
-// ,'ipspiderws5'=>array('ep'=>'https://ipspiderws5.herokuapp.com/ep.php')
-// ,'ipspiderws6'=>array('ep'=>'https://ipspiderws6.herokuapp.com/ep.php')
+,'ipspiderws2'=>array('ep'=>'https://ipspiderws2.herokuapp.com/ep.php')
+,'ipspiderws3'=>array('ep'=>'https://ipspiderws3.herokuapp.com/ep.php')
+,'ipspiderws4'=>array('ep'=>'https://ipspiderws4.herokuapp.com/ep.php')
+,'ipspiderws5'=>array('ep'=>'https://ipspiderws5.herokuapp.com/ep.php')
+,'ipspiderws6'=>array('ep'=>'https://ipspiderws6.herokuapp.com/ep.php')
 );
 ?>
